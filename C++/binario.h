0ed70eb03c6441d96492a677854e8ad32e17e949
Arthur Oliveira <39603069+ArthurOlive@users.noreply.github.com>
2019-04-07 12:32:19 -0300

    Rename binario.h to C++/binario.h

---
+++ C++/binario.h
@@ -0,0 +1,9 @@
+#pragma once
+//funções do binario.cpp
+unsigned short andBit(unsigned short, unsigned short);	//função AND
+unsigned short orBit(unsigned short, unsigned short);	//função XOR 
+unsigned short ligBit(unsigned short, unsigned short);	//função liga Bits
+unsigned short desBit(unsigned short, unsigned short);	//função desliga Bits
+bool testBit(unsigned short, unsigned short);			//função teste Bits
+unsigned short lowBit(unsigned short);					//função Bits baixos
+unsigned short hightBit(unsigned short);				//função Bits altos
